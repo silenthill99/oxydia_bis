ec2c5299a6c4355ca04c3c7cebfd91a2d61d62f9
silenthill99 <florian.graziani@sfr.fr>
2024-12-18 18:47:17 +0100

    Modification de la mise en page

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('accueil');
+Route::get('/', [WelcomeController::class, 'index'])->name('accueil');
 
 Route::get("/projects", function () {
     return Inertia::render("Nos projets");
