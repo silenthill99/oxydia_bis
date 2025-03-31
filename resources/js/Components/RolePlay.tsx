08c052ee609716d9ef277f97892a7eef5d6ae092
BuildTools <unconfigured@null.spigotmc.org>
2025-03-31 16:55:49 +0200

    Déplacement des images dans le dossier public

--- resources/js/Components/RolePlay.tsx
+++ resources/js/Components/RolePlay.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Mairie from "../../images/Mairie.png"
-import FranceTravail from "../../images/France travail.png"
-import Bibliotheque from "../../images/Bibliothèque.png"
 
 type Images = {
     src: string;
@@ -9,27 +6,27 @@ type Images = {
     desc: string;
 }
 
-const ImageList: Images[] = [{
-    src: Mairie,
+const imageList: Images[] = [{
+    src: "/img/Mairie.png",
     alt: "Mairie",
     desc: "Mairie"
 }, {
-    src: FranceTravail,
+    src: "/img/France-travail.png",
     alt: "France travail",
     desc: "France travail"
 }, {
-    src: Bibliotheque,
+    src: "/img/Bibliothèque.png",
     alt: "Bibliothèque",
     desc: "Bibliothèque municipale"
 }]
 
-const RolePlay = () => {
+const RolePlay : React.FC = () => {
     return (
         <>
             <h2>RolePlay</h2>
             <div className={"grid md:grid-cols-3 gap-5 m-5 md:m-0"}>
-                {ImageList.map((image, id) => (
-                    <a key={id} href={image.src} target={"_blank"} className={"bg-white border border-slate-300 rounded shadow md:hover:scale-125 duration-300 flex flex-col"}>
+                {imageList.map((image, index) => (
+                    <a key={index} href={image.src} target={"_blank"} className={"bg-white border border-slate-300 rounded shadow md:hover:scale-125 duration-300 flex flex-col"}>
                         <strong className={"text-center w-full inline-block"}>{image.desc}</strong>
                         <img src={image.src} className={"w-full h-full object-cover"} alt={image.alt} />
                     </a>
