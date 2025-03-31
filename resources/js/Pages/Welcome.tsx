84ff801cb113f16c6d76e13735337712ea6c90d5
BuildTools <unconfigured@null.spigotmc.org>
2025-03-31 17:51:26 +0200

    Suppression d'éléments superflux

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -9,11 +9,6 @@ export default function Welcome({auth}: PageProps) {
             <h1 style={{textDecorationLine: "underline overline"}} className={"indent-1"}>Page d'accueil</h1>
             <p>Bienvenue sur le site principal de l'infrastructure Oxydia</p>
             <p>Oxydia est un projet de network ayant pour but de regrouper divers serveurs Minecraft.</p>
-
-            <h1 style={{textShadow: "5px 5px"}}>Un titre de niveau 1</h1>
-
-            <p style={{textShadow: "-5px 2px 1px blue"}}>Un premier paragraphe</p>
-            <p style={{textShadow: "0 0 5px red"}}>Un autre paragraphe</p>
         </PageComponents>
     )
 }
