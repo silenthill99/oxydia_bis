e2e0dfd2efaa4e843fd686b581824ea9a96f8c94
silenthill99 <florian.graziani@sfr.fr>
2026-01-28 13:19:13 +0100

    Mise à jour des rôles

--- app/Enums/RoleEnum.php
+++ app/Enums/RoleEnum.php
@@ -5,12 +5,14 @@ namespace App\Enums;
 enum RoleEnum: string
 {
     case PLAYER = "player";
+    case RESPONSABLE = "responsable";
     case ADMIN = "admin";
 
     public function label(): string
     {
         return match ($this) {
             self::PLAYER => "Joueur",
+            self::RESPONSABLE => "Responsable d'équipe",
             self::ADMIN => "Administrateur",
         };
 
--- database/migrations/2026_01_13_172333_create_roles_table.php
+++ database/migrations/2026_01_13_172333_create_roles_table.php
@@ -5,6 +5,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -24,8 +25,15 @@ return new class extends Migration
             $table->foreignIdFor(Role::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
         });
-    }
 
+        foreach (RoleEnum::cases() as $role) {
+            DB::table('roles')->insert([
+                'role' => $role->value,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
     /**
      * Reverse the migrations.
      */
